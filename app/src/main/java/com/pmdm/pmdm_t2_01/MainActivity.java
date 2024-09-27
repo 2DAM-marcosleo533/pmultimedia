0b8c63125cf953820e2a9a26104a2fa14b3a3bf5
unknown <2DAM-marcosleo533@IESMARTINEZM.ES>
2024-09-27 20:31:53 +0200

    Android

--- app/src/main/java/com/pmdm/pmdm_t2_01/MainActivity.java
+++ app/src/main/java/com/pmdm/pmdm_t2_01/MainActivity.java
@@ -1,6 +1,7 @@
 package com.pmdm.pmdm_t2_01;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -11,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+    private final String TAG = "PMOM";
+    private TextToSpeech sintetizador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +30,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    sintetizador = new TextToSpeech(this, this);
     }
-
-    Toast.makeText(this, "se crea la app", Toast.LENGTH_LONG).show();
-
-
     @Override
     protected void onPostResume() {
         super.onPostResume();
         Toast.makeText(this, "se crea la app", Toast.LENGTH_LONG).show();
+        hablar("Vuelve a la app");
     }
 
     @Override
@@ -44,17 +50,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Toast.makeText(this, "se crea la app", Toast.LENGTH_LONG).show();
+        sintetizador.shutdown();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Toast.makeText(this, "se crea la app", Toast.LENGTH_LONG).show();
+        hablar("Empieza la app");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Toast.makeText(this, "se crea la app", Toast.LENGTH_LONG).show();
+    }
+
+    private void hablar(String msg){
+        sintetizador.speak(msg, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    //Parametros de configuracion del sintetizador
+    @Override
+    public void onInit(int i) {
+
+    sintetizador.setLanguage(Locale.getDefault());
+    sintetizador.setPitch(1f);
+    sintetizador.setSpeechRate(1f);
+
     }
 }
